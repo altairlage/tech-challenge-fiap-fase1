713b38878408148cfdbc85ec70abb74a8031c83a
Thiago Leite <thiago.0686@gmail.com>
2025-01-11 20:06:13 -0300

    create update user

--- GastroHub/src/main/java/com/fiap/GastroHub/modules/users/usecases/GetUserByIdUseCase.java
+++ GastroHub/src/main/java/com/fiap/GastroHub/modules/users/usecases/GetUserByIdUseCase.java
@@ -4,19 +4,16 @@ import com.fiap.GastroHub.modules.users.dtos.UserResponse;
 import com.fiap.GastroHub.modules.users.infra.orm.entities.User;
 import com.fiap.GastroHub.modules.users.infra.orm.repositories.UserRepository;
 import com.fiap.GastroHub.shared.AppException;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class GetUserByIdUseCase {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
-
-    public GetUserByIdUseCase(UserRepository userRepository, ModelMapper modelMapper) {
-        this.userRepository = userRepository;
-        this.modelMapper = modelMapper;
-    }
 
     public UserResponse execute(Long id) {
         User user = userRepository.findById(id)
