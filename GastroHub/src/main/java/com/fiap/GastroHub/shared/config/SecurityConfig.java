ac91c1b7a0683374434cdd892043ec6c8f061120
Gaddini <ferreiragaddini@bol.com.br>
2025-01-14 15:29:43 -0300

    Feature Doc Swagger: Ajustado SecurityConfig para liberação da swagger-ui

--- GastroHub/src/main/java/com/fiap/GastroHub/shared/config/SecurityConfig.java
+++ GastroHub/src/main/java/com/fiap/GastroHub/shared/config/SecurityConfig.java
@@ -3,24 +3,13 @@ package com.fiap.GastroHub.shared.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/swagger-ui-custom.html/**", "/v3/api-docs/**", "/swagger-ui-custom.html",
-                                "/swagger-ui/index.html").permitAll()
-                        .anyRequest().authenticated()
-                );
-        return http.build();
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs*/**");
     }
 }
